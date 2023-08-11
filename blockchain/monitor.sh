83f932ec070215ff32b6f4a0620709772b3dcd52
h.khaleghirad@gmail.com <h.khaleghirad@gmail.com>
2023-08-11 23:45:06 +0330

    blockchain and ntru seperatly are OK

--- blockchain/monitor.sh
+++ blockchain/monitor.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-watch -n 2 java -cp target/simple-chain-0.0.1-SNAPSHOT.jar be.cylab.simplechain.Monitor
+watch -n 2 java -cp target/blockchain-1.0-SNAPSHOT.jar be.cylab.simplechain.Monitor
